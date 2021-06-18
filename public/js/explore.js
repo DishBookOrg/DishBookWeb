ab8a9bbaa35c05c2bb5770501a3c5e93ae6d3b34
Dinesko <borozenetsdenys@gmail.com>
2021-06-18 20:41:23 +0300

    add render for try it block

--- public/js/explore.js
+++ public/js/explore.js
@@ -22,10 +22,55 @@
 //     });
 // });
 
-db.collection('PublicDishes').get().then((snapshot) => {
-    snapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
+const tryItDiv = document.querySelector('#dishes-cards')
+
+function renderBigCard(doc){
+   
+    let bigCard = document.createElement('div');
+    bigCard.classList.add("big-card");
+    let bigCardText = document.createElement('p');
+    bigCardText.classList.add("big-card-text");
+    let bigCardImg = document.createElement('div');
+    bigCardImg.classList.add("big-card-img");
+    let bigCardTextBlock = document.createElement('div');
+    bigCardTextBlock.classList.add("big-card-text-block");
+    let bigCardTextInside = document.createElement('p');
+    bigCardTextInside.classList.add("big-card-text-inside");
+    let imageTime = document.createElement('img');
+    imageTime.src = "./img/time.svg";
+    let timeTextBig = document.createElement('p');
+    timeTextBig.classList.add("time-text-big");
+
+    bigCardText.textContent = "Try it!";
+
+    bigCardTextBlock.appendChild(bigCardTextInside);
+    bigCardTextBlock.appendChild(imageTime);
+    bigCardTextBlock.appendChild(timeTextBig);
+    bigCardImg.appendChild(bigCardTextBlock);
+
+    bigCard.appendChild(bigCardText);
+    bigCard.appendChild(bigCardImg);
+
+    bigCardTextInside.textContent = doc.data().dishName;
+    timeTextBig.textContent = doc.data().dishTotalTime;
+
+    var httpsReference = storage.refFromURL(doc.data().dishImageURL);
+    console.log(httpsReference);
+    httpsReference.getDownloadURL().then(function(url) {
+        console.log('Got download URL');
+        console.log(url);
+        bigCardImg.style.backgroundImage = "url(" + url + ")";
     });
-   }).catch(err => {
-     console.log(err);
-   });
+    tryItDiv.appendChild(bigCard);
+}
+
+db.collection('PublicDishes')
+    .limit(1)
+    .get()
+    .then((snapshot) => { snapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+        renderBigCard(doc);
+    });
+    }).catch(err => {
+        console.log(err);
+    });
